7586aeea03b763d008b5e024e46d0bdcce125cd3
Faisal <falquaddoomi@ucla.edu>
2018-06-11 14:17:06 +0200

    Integrated SilicoPredTile into VariantDetail column definitions, renderer.

--- website/js/index.js
+++ website/js/index.js
@@ -5,6 +5,7 @@
 // shims for older browsers
 import SourceReportsTile from "./components/SourceReportsTile";
 import AlleleFrequenciesTile from "./components/AlleleFrequenciesTile";
+import SilicoPredTile from "./components/insilicopred/SilicoPredTile";
 
 require('babel/polyfill');
 require('es5-shim');
@@ -703,7 +704,7 @@ var VariantDetail = React.createClass({
         let groupsEmpty = 0;
         let totalRowsEmpty = 0;
 
-        const groupTables = _.map(groups, ({ groupTitle, innerCols, reportSource, reportBinding, alleleFrequencies, innerGroups }) => {
+        const groupTables = _.map(groups, ({ groupTitle, innerCols, reportSource, reportBinding, alleleFrequencies, inSilicoPred, innerGroups }) => {
             let rowsEmpty = 0;
 
             // if it's a report source (i.e. the key reportSource is defined), then we defer
@@ -761,6 +762,20 @@ var VariantDetail = React.createClass({
                         showHelp={this.showHelp}
                         tooltips={this.state.tooltips}
                         variant={variant}
+                    />
+                );
+            }
+
+            if (inSilicoPred) {
+                return (
+                    <SilicoPredTile
+                        groupTitle={groupTitle}
+                        onChangeGroupVisibility={this.onChangeGroupVisibility}
+                        hideEmptyItems={this.state.hideEmptyItems}
+                        onDimsChanged={() => {
+                            setTimeout(() => { this.forceUpdate(); }, 0);
+                        }}
+                        showHelp={this.showHelp}
                     />
                 );
             }
