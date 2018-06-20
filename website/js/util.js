289ff116abefc0aadbdff3673e0a5973471fcd56
Faisal <falquaddoomi@ucla.edu>
2018-06-20 15:36:05 +0200

    SilicoPredTile now joins current variant against mock in-silico predication data on the genomic coordinate hg38 field. Moved some helper funcs into the main util module.

--- website/js/util.js
+++ website/js/util.js
@@ -51,6 +51,10 @@ function isEmptyField(value) {
     return v === '' || v === '-' || v === 'None';
 }
 
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 
 // attempts to parse the given date string using a variety of formats,
 // returning the formatted result as something like '08 September 2016'.
@@ -200,6 +204,11 @@ function sentenceCase(str) {
     return str.replace(/\b\S/g, (t) => t.toUpperCase() );
 }
 
+function capitalize(w) {
+    return w.charAt(0).toUpperCase() + w.substr(1);
+}
+
+
 function getAminoAcidCode(hgvsProtein) {
     let trimmedHgvs = hgvsProtein.replace(/[0-9()]/g, '');
     if (trimmedHgvs.length < 3) {
@@ -218,6 +227,7 @@ function getAminoAcidCode(hgvsProtein) {
 module.exports = {
     getAminoAcidCode,
     isEmptyField,
+    isNumeric,
     normalizeDateFieldDisplay,
     normalizedFieldDisplay,
     generateLinkToGenomeBrowser,
@@ -225,5 +235,6 @@ module.exports = {
     abbreviatedSubmitter,
     sentenceCase,
     reformatDate,
-    dateKeys
+    dateKeys,
+    capitalize
 };
